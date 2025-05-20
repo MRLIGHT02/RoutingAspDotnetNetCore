532d05c0720f855eeef462e260f978b685fe0344
MRLIGHT02 <saifali42020@gmail.com>
2025-05-20 17:05:00 +0530

    #

--- RoutingAspDotnetNetCore/Program.cs
+++ RoutingAspDotnetNetCore/Program.cs
@@ -2,20 +2,24 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseRouting();
-//app.UseEndpoints(endpoints =>
-//{
-//    // add your own endpoint
-//    endpoints.Map("map1", async (context) =>
-//    {
-//        await context.Response.WriteAsync("Map 1");
-//    });
-
-//});
-
-app.Map("map1", async(HttpContext context) =>
+app.UseEndpoints(endpoints =>
 {
-    await context.Response.WriteAsync("Welcom to Map 1");
+    // add your own endpoint
+    endpoints.Map("map1", async (context) =>
+    {
+        await context.Response.WriteAsync("Map 1");
+    });
+    endpoints.Map("map2", async (context) =>
+    {
+        await context.Response.WriteAsync("Welcome to Map 2");
+    });
+
 });
+
+//app.Map("map1", async(HttpContext context) =>
+//{
+//    await context.Response.WriteAsync("Welcom to Map 1");
+//});
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
