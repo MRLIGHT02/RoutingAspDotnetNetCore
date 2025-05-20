ef985ca58335351f24effbf2062e78a3a0309424
MRLIGHT02 <saifali42020@gmail.com>
2025-05-20 15:46:11 +0530

    #
    Refactor endpoint mapping in Program.cs
    
    Updated endpoint configuration to use app.Map instead of
    app.UseEndpoints. Changed response message from "mr light"
    to "Welcome to Map 1" for improved clarity.

--- RoutingAspDotnetNetCore/Program.cs
+++ RoutingAspDotnetNetCore/Program.cs
@@ -2,14 +2,19 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseRouting();
-app.UseEndpoints(endpoints =>
-{
-    // add your own endpoint
-    endpoints.Map("map1", async (context) =>
-    {
-        await context.Response.WriteAsync("mr light");
-    });
+//app.UseEndpoints(endpoints =>
+//{
+//    // add your own endpoint
+//    endpoints.Map("map1", async (context) =>
+//    {
+//        await context.Response.WriteAsync("Map 1");
+//    });
 
+//});
+
+app.Map("map1", async(HttpContext context) =>
+{
+    await context.Response.WriteAsync("Welcom to Map 1");
 });
 app.MapGet("/", () => "Hello World!");
 
