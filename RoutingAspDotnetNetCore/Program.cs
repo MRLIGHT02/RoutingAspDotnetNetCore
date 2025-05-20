9b7e036e62c9754ca8d97a552aeed49f3e5ed720
MRLIGHT02 <saifali42020@gmail.com>
2025-05-20 15:34:35 +0530

    #

--- RoutingAspDotnetNetCore/Program.cs
+++ RoutingAspDotnetNetCore/Program.cs
@@ -2,7 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseRouting();
-app.UseEndpoints();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.Map("/map1", async (HttpContext context, RequestDelegate next) =>
+    {
+        await context.Response.WriteAsync("hello world");
+    });
+
+});
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
