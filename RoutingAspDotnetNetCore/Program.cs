6b255b5eef31cde7f41586c9c6b3506c7c6a1da8
MRLIGHT02 <saifali42020@gmail.com>
2025-05-30 14:34:40 +0530

    #

--- RoutingAspDotnetNetCore/Program.cs
+++ RoutingAspDotnetNetCore/Program.cs
@@ -9,18 +9,13 @@ app.UseRouting();
 
 app.UseEndpoints(endpoints =>
 {
-    endpoints.Map("files/{filename}.{extension}", async context =>
-    {
-        await context.Response.WriteAsync("In files");
-    });
+
 
 });
 
 
-//app.Map("map1", async(HttpContext context) =>
-//{
-//    await context.Response.WriteAsync("Welcom to Map 1");
-//});
+
+
 app.Run(async context =>
 {
     await context.Response.WriteAsync($"Request recieved  at {context.Request.Path}");
